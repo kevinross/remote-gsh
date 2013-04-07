86f75f54e90c0cc99d070accb25373c032ceab31
safrain <z.safrain@gmail.com>
2013-04-08 02:06:31 +0800

    Add copyrights

--- remote-gsh-example/examples.groovy
+++ remote-gsh-example/examples.groovy
@@ -1,16 +1,4 @@
 /*
- * Remote Groovy Shell    A servlet web application management tool
- *   Copyright (c)          2013 Safrain <z.safrain@gmail.com>
- *                          All Rights Reserved
- *
- *   This program is free software. It comes without any warranty, to
- *   the extent permitted by applicable law. You can redistribute it
- *   and/or modify it under the terms of the Do What The Fuck You Want
- *   To Public License, Version 2, as published by Sam Hocevar. See
- *   http://www.wtfpl.net/ for more details.
- */
-
-/*
   This is Remote Groovy Shell example script
   It will show you what Remote Groovy Shell can do with a web application
 
--- remote-gsh-shell/src/main/groovy/com/github/safrain/remotegsh/shell/GshShell.groovy
+++ remote-gsh-shell/src/main/groovy/com/github/safrain/remotegsh/shell/GshShell.groovy
@@ -1,3 +1,15 @@
+/*
+ * Remote Groovy Shell    A servlet web application management tool
+ * Copyright (c)          2013 Safrain <z.safrain@gmail.com>
+ *                        All Rights Reserved
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
 package com.github.safrain.remotegsh.shell
 
 import groovy.json.JsonSlurper
